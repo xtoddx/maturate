08ce28b382d861c10f84e3724f35ef7c5cebe6f0
Todd Willey <xtoddx@gmail.com>
2015-10-17 23:58:31 -0400

    isolate test controller, test current_version goodness

--- test/maturate_test.rb
+++ test/maturate_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class MaturateTest < ActiveSupport::TestCase
+  setup do
+    @controller = Class.new(ActionController::Base) { extend Maturate }
+  end
+
   test 'can set api version on controller' do
-    assert ApplicationController.respond_to?(:api_versions=)
+    assert @controller.respond_to?(:api_versions=)
+  end
+
+  test 'can set current_api_version to a known version' do
+    @controller.api_versions = ['a', 'b']
+    assert_send [@controller, :current_api_version=, 'b']
+  end
+
+  test 'cannot set current_api_version to an unknown version' do
+    @controller.api_versions = ['a', 'b']
+    assert_raises(Maturate::InvalidVersion) do
+      @controller.current_api_version = 'c'
+    end
   end
 end
